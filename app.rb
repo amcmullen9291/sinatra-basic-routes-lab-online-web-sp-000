1426c58507122037be607a1ad1f6c06c22e3c227
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-26 11:32:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  get '/' do
+    "Hello, World!"
+  end
   
+  get '/name' do
+    "Hello, World!"
+  end
+
+  get '/hometown' do
+    "Hello, World!"
+  end
+
+  get '/favorite-song' do
+    "Hello, World!"
+  end
+
 end
